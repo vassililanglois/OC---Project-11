522337667258b8cfdaf008181a0234962c518b7f
Vassili <vassililanglois@icloud.com>
2025-05-15 11:31:17 +0200

    feat(all pages): making all the responsive for each pages.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+
 function Banner() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="banner">
-      <h1>Chez vous, partout et ailleurs</h1>
+      <h1>
+        Chez vous,
+        {isMobile ? <br /> : " "}
+        partout et ailleurs
+      </h1>
     </div>
   );
 }
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -8,7 +8,6 @@ function Location() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [location, setLocation] = useState(null);
-  const [rating, setRating] = useState(0);
 
   useEffect(() => {
     const foundLocation = getLocationById(id);
@@ -22,10 +21,6 @@ function Location() {
 
   // Si location est null (avant la redirection), on retourne rien ou un loading
   if (!location) return null;
-
-  const handleRatingClick = (ratingValue) => {
-    setRating(ratingValue);
-  };
 
   return (
     <div className="location">
@@ -57,11 +52,10 @@ function Location() {
                 viewBox="0 0 36 36"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                onClick={() => handleRatingClick(star)}
               >
                 <path
                   d="M19.7212 6.84366C19.4728 6.32809 18.9479 6 18.3714 6C17.7949 6 17.2747 6.32809 17.0216 6.84366L14.0079 13.0445L7.27748 14.0382C6.71506 14.1225 6.24636 14.5162 6.07295 15.0552C5.89953 15.5942 6.04014 16.1895 6.44321 16.5879L11.327 21.4201L10.174 28.2491C10.0803 28.8115 10.3146 29.3833 10.7786 29.7161C11.2426 30.0489 11.8566 30.0911 12.3628 29.8239L18.3761 26.6133L24.3894 29.8239C24.8956 30.0911 25.5096 30.0536 25.9736 29.7161C26.4376 29.3786 26.6719 28.8115 26.5782 28.2491L25.4205 21.4201L30.3043 16.5879C30.7073 16.1895 30.8526 15.5942 30.6745 15.0552C30.4964 14.5162 30.0324 14.1225 29.47 14.0382L22.7349 13.0445L19.7212 6.84366Z"
-                  fill={rating >= star ? "#ff6060" : "#E3E3E3"}
+                  fill={location.rating >= star ? "#ff6060" : "#E3E3E3"}
                 />
               </svg>
             ))}
