be301d26b76768939a6015e5dd430e7028c9c373
Vassili <vassililanglois@icloud.com>
2025-06-03 20:23:53 +0200

    feat(routing): separate the routing into a folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
-import Banner from "./components/Banner";
-import Home from "./pages/Home";
-import Location from "./pages/Location";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Location />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/notfound" element={<NotFound />} />
-      </Routes>
+      <AppRoutes />
       <Footer />
     </div>
   );
